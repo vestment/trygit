9940ec849bc2d31e8f637caeec71d1e89d342913
Marco <marco.mouns@gmail.com>
2018-09-27 14:39:59 +0200

    Auto stash before merge of "master" and "origin/master"
    first comment

--- app/src/main/java/com/example/vestment23/trygit/MainActivity.java
+++ app/src/main/java/com/example/vestment23/trygit/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView helloWorld = findViewById(R.id.hello);
 
-        helloWorld.setText("welcome !");
+        helloWorld.setText("welcomesssssssssssssss !");
     }
 }
