b0655fdbf9c663aafd02cc56e4d00abeb880c231
Marco <marco.mouns@gmail.com>
2018-09-27 15:22:27 +0200

    new updated

--- app/src/main/java/com/example/vestment23/trygit/SecondActivity.java
+++ app/src/main/java/com/example/vestment23/trygit/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.vestment23.trygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.firsttextview);
+        textView.setText("laplapalplaplap");
     }
 }
